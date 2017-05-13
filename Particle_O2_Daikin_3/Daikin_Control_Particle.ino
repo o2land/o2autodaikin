3848a56e72a550f6c224b7e1d8878a52d4fddb78
Samson Chen <samson@qblinks.com>
2017-05-13 09:04:03 +0800

    raised temperature criteria a little bit

--- Particle_O2_Daikin_3/Daikin_Control_Particle.ino
+++ Particle_O2_Daikin_3/Daikin_Control_Particle.ino
@@ -26,12 +26,12 @@
 //
 
 // init log information
-#define INIT_STR                "RhT Control System Initialized V3.1, 2017-05-07"
+#define INIT_STR                "RhT Control System Initialized V3.1, 2017-05-13"
 
 // ambient environmental parameters
 #define DEH_TEMP                26.00       // This is (TEMP_AC_CMD + 2) because AC may stop running below that point
-#define COLD_TEMP_HI            24.10       // use Heat Index
-#define COLD_TEMP_HI_H          24.55       // use Heat Index (Higher Tempeature Period)
+#define COLD_TEMP_HI            24.20       // use Heat Index
+#define COLD_TEMP_HI_H          24.60       // use Heat Index (Higher Tempeature Period)
 
 #define TEMP_AC_CMD             "att24"
 #define TEMP_BOOST_CMD          "att23"
